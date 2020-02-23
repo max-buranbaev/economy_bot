0114a81679c228f135c3ad5c07e5f37de1324aba
Max Buranbaev <max@izst.ru>
2020-02-23 18:58:51 +0100

    feat(bot): hook reimplementation

--- handler.ts
+++ handler.ts
@@ -1,17 +1,18 @@
 'use strict';
-import { initiateBot } from './src/bot';
+import { sendToUser } from './src/bot';
 
 module.exports.bot = async event => {
-  initiateBot();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: '✅ Bot is started ',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  const body = JSON.parse(event.body);
+  const {chat, text} = body.message;
+  try {
+    if(text) {
+      await sendToUser(chat, text);
+    }
+    return { statusCode: 200 }
+  } catch(e) {
+    return {
+      statusCode: 500,
+      text: e
+    }
+  }
 };
--- src/bot.ts
+++ src/bot.ts
@@ -1,20 +1,16 @@
-import * as TelegramBot from "node-telegram-bot-api";
+import * as rp from 'request-promise';
 import * as flow from "dotenv-flow";
 
 flow.config();
-export function initiateBot() {
-    const bot = new TelegramBot("765715761:AAHQujjcLGjy7xt5rV07mVo43TrJr7I3fLM", {
-        polling: true
-      });
-      
-      bot.onText(/\/echo (.+)/, (msg, match) => {
-        const chatId = msg.chat.id;
-        const resp = match[1];
-        bot.sendMessage(chatId, resp);
-      });
-      
-      bot.on("message", msg => {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, "Received your message");
-      });      
+export async function sendToUser(chat_id, text) {
+  const options = {
+    method: 'GET',
+    uri: `https://api.telegram.org/bot765715761:AAHQujjcLGjy7xt5rV07mVo43TrJr7I3fLM/sendMessage`,
+    qs: {
+      chat_id,
+      text
+    }
+  };
+
+  return rp(options);
 }
